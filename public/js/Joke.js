6d51d772bf5a3107c8d2780cd8f37d0f86b5349f
Sophie <s.rivoyre@gmail.com>
2020-09-03 12:09:33 +0200

    #22 Adding XHR class

--- public/js/Joke.js
+++ public/js/Joke.js
@@ -11,5 +11,4 @@ class Joke {
         this.setup = param.setup;
         this.delivery = param.delivery;
     }
-
 }
---
+++ public/js/XHRRequest.js
@@ -0,0 +1,24 @@
+class XHRRequest {
+
+    constructor(callback, p_idRange = 1-100) {
+        this.baseURL = 'https://sv443.net/jokeapi/v2';
+        this.categories = ['Programming', 'Miscellaneous', 'Pun'];
+        this.params = [
+            'blacklistFlags=nsfw,religious,racist',
+            'idRange=' + p_idRange
+        ];
+        this.xhr = new XMLHttpRequest();
+        this.xhr.open("GET", this.baseURL + "/joke/" + this.categories.join(",") + "?" + this.params.join("&"));
+        this.xhr.addEventListener('load', () => {
+            if (this.xhr.readyState == 4 && this.xhr.status < 300) // readyState 4 means request has finished + we only want to parse the joke if the request was successful (status code lower than 300)
+            {
+                callback(this.xhr.responseText);
+
+            } else if (this.xhr.readyState == 4) {
+                console.log("Error while requesting joke.\n\nStatus code: " + this.xhr.status + "\nServer response: " + this.xhr.responseText);
+            }
+
+        });
+        this.xhr.send();
+    }
+}
