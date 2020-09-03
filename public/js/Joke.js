c293e660e4d05626b04f7e1ebb0f6f4bfce86dd8
Sophie <s.rivoyre@gmail.com>
2020-09-03 12:06:31 +0200

    #22 Adding Joke class

---
+++ public/js/Joke.js
@@ -0,0 +1,15 @@
+class Joke {
+
+    constructor(param) {
+        this.category = param.category;
+        this.error = param.error ;
+        this.flags = param.flags;
+        this.id = param.id;
+        this.joke = param.joke;
+        this.lang = param.lang;
+        this.type = param.type;
+        this.setup = param.setup;
+        this.delivery = param.delivery;
+    }
+
+}
