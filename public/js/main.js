c918f2609e3973e19f43becb00a5a0d1a5b8356b
Sophie <s.rivoyre@gmail.com>
2020-09-11 14:27:32 +0200

    #42 Adding client form validation with js

--- public/js/main.js
+++ public/js/main.js
@@ -8,17 +8,117 @@ let filteredJokesArray;
  * standard functions
  ************************************/
 function hideElement(element) {
-    if (!element.classList.contains('d-none')) {
+    /*if (!element.classList.contains('d-none')) {
         element.classList.add('d-none');
-    }
+    }*/
+    addClass(element, 'd-none');
 }
 
 function showElement(element) {
-    if (element.classList.contains('d-none')) {
+    /*if (element.classList.contains('d-none')) {
         element.classList.remove('d-none');
+    }*/
+    removeClass(element, 'd-none')
+}
+
+function addClass(element, className) {
+    if (!element.classList.contains(className)) {
+        element.classList.add(className);
     }
 }
 
+function removeClass(element, className) {
+    if (element.classList.contains(className)) {
+        element.classList.remove(className);
+    }
+}
+
+/****************************************
+ * Forms validation
+ ****************************************/
+function checkUserInput() {
+    checkPseudo();
+    checkEmail();
+    checkPassword();
+    checkFormSubmission();
+}
+
+function checkPseudo() {
+    let pseudo = document.getElementById('pseudo');
+
+    if (pseudo !== null) {
+        pseudo.addEventListener('input', (e) => {
+            return checkInputMinLength(pseudo, e.target.value, 3);
+        });
+    }
+}
+
+function checkEmail() {
+    let email = document.getElementById('email');
+
+    if (email !== null) {
+        email.addEventListener('input', (e) => {
+            const emailRegex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+(?:[A-Z]{2}|com|org|net|gov|mil|biz|info|mobi|name|aero|jobs|museum)\b/;
+
+            if (emailRegex.test(e.target.value) === true) {
+                return validateInput(email, true);
+            } else {
+                return validateInput(email, false);
+            }
+        });
+    }
+}
+
+function checkPassword() {
+    let password = document.getElementById('password');
+    let newPassword = document.getElementById('newPassword');
+
+    if (password !== null) {
+        password.addEventListener('input', (e) => {
+            return checkInputMinLength(password, e.target.value, 6);
+        });
+    }
+    if (newPassword !== null) {
+        newPassword.addEventListener('input', (e) => {
+            return checkInputMinLength(newPassword, e.target.value, 6);
+        });
+    }
+}
+
+function checkInputMinLength(elt, input, length) {
+    if (input.length < length) {
+        return validateInput(elt, false);
+    } else {
+        return validateInput(elt, true);
+    }
+}
+
+function validateInput(elt, validInput) {
+    if (validInput === false) {
+        addClass(elt, 'is-invalid');
+        removeClass(elt, 'is-valid');
+        return false;
+    } else {
+        addClass(elt, 'is-valid');
+        removeClass(elt, 'is-invalid')
+        return true;
+    }
+}
+
+function checkFormSubmission() {
+    'use strict';
+    let forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    let validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(e) {
+            if (form.checkValidity() === false) {
+                e.preventDefault();
+                e.stopPropagation();
+            }
+            form.classList.add('was-validated');
+        }, false);
+    });
+}
 /****************************************
  * App functions
  ****************************************/
@@ -69,23 +169,7 @@ const processResult = function (result, random) {
         joke.createJokeContent();
     }
     if (random === true) {
-        manageActionButtons(
-            document.getElementById('saveJokeBtn'),
-            document.getElementById('removeSavedJokeBtn'),
-            joke.id);
         showElement(document.getElementById('joke-container'));
-    }
-}
-
-function manageActionButtons(saveBtn, unsaveBtn, jokeId) {
-    if( savedJokesArray !== null
-        && savedJokesArray.indexOf(jokeId.toString()) === -1
-    ) {
-        hideElement(unsaveBtn);
-        showElement(saveBtn);
-    } else {
-        hideElement(saveBtn);
-        showElement(unsaveBtn);
     }
 }
 
