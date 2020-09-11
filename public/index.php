37ed2ac6e5cf1e3d39a6ea63511e72da6c4c0401
Sophie <s.rivoyre@gmail.com>
2020-09-11 21:02:25 +0200

    Removing debugger for production

--- public/index.php
+++ public/index.php
@@ -1,8 +1,9 @@
 <?php
 require_once '../config/dev.php';
 require_once '../vendor/autoload.php';
-use Tracy\Debugger;
-Debugger::enable();
+// Deactivated for prod environment
+//use Tracy\Debugger;
+//Debugger::enable();
 session_start();
 $router = new App\Src\Router();
 $router->run();
