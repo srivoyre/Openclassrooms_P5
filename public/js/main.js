316efb77d7cc8df0a4df01d4bd182e14b17190b9
Sophie <s.rivoyre@gmail.com>
2020-09-11 14:35:23 +0200

    Refactoring password check function

--- public/js/main.js
+++ public/js/main.js
@@ -7,20 +7,6 @@ let filteredJokesArray;
 /*************************************
  * standard functions
  ************************************/
-function hideElement(element) {
-    /*if (!element.classList.contains('d-none')) {
-        element.classList.add('d-none');
-    }*/
-    addClass(element, 'd-none');
-}
-
-function showElement(element) {
-    /*if (element.classList.contains('d-none')) {
-        element.classList.remove('d-none');
-    }*/
-    removeClass(element, 'd-none')
-}
-
 function addClass(element, className) {
     if (!element.classList.contains(className)) {
         element.classList.add(className);
@@ -31,6 +17,14 @@ function removeClass(element, className) {
     if (element.classList.contains(className)) {
         element.classList.remove(className);
     }
+}
+
+function hideElement(element) {
+    addClass(element, 'd-none');
+}
+
+function showElement(element) {
+    removeClass(element, 'd-none')
 }
 
 /****************************************
@@ -72,15 +66,17 @@ function checkEmail() {
 function checkPassword() {
     let password = document.getElementById('password');
     let newPassword = document.getElementById('newPassword');
+    let eltToCheck;
 
-    if (password !== null) {
-        password.addEventListener('input', (e) => {
-            return checkInputMinLength(password, e.target.value, 6);
-        });
-    }
     if (newPassword !== null) {
-        newPassword.addEventListener('input', (e) => {
-            return checkInputMinLength(newPassword, e.target.value, 6);
+        eltToCheck = newPassword;
+    } else {
+        eltToCheck = password;
+    }
+
+    if (eltToCheck !== null) {
+        eltToCheck.addEventListener('input', (e) => {
+            return checkInputMinLength(eltToCheck, e.target.value, 6);
         });
     }
 }
